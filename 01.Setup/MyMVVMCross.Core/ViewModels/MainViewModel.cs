42336334488c7f138d7cc5836cc1f9962c3c22be
RobertShayne <realshayne@gmail.com>
2019-01-29 12:26:06 +0800

    s

---
+++ 01.Setup/MyMVVMCross.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMVVMCross.Core.ViewModels
+{
+    public class MainViewModel : MvxViewModel
+    {
+
+    }
+}
